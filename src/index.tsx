4815d7996b79c084777b87f66f1709fc7ef68898
sweenydev <sweenydev@gmail.com>
2023-06-02 18:45:30 -0700

    IT's ALIVER (more firebase hosting)

--- src/index.tsx
+++ src/index.tsx
@@ -3,6 +3,21 @@ import ReactDOM from 'react-dom/client';
 import './index.scss';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
+import { initializeApp } from "firebase/app";
+import { getAnalytics } from "firebase/analytics";
+
+const firebaseConfig = {
+  apiKey: "AIzaSyCI5pnnoKyC9g1AHSN0qq_xkGc3bOaUSPk",
+  authDomain: "word-player-sweeny.firebaseapp.com",
+  projectId: "word-player-sweeny",
+  storageBucket: "word-player-sweeny.appspot.com",
+  messagingSenderId: "904947896904",
+  appId: "1:904947896904:web:0a2085a0fed5025e153e7d",
+  measurementId: "G-ZHFWCJ21E1"
+};
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+const analytics = getAnalytics(app);
 
 const root = ReactDOM.createRoot(
   document.getElementById('root') as HTMLElement
